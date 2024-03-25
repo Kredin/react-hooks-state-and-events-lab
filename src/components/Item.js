7f54d2093d10870b1d39a76a0b583861699ea72d
Blade <bladeprojectz@gmail.com>
2024-03-25 09:19:29 +0800

    lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [added, setAdded] = useState(false);
+  const itemClass = added ? "remove" : "add";
+  const liClass = added ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className={itemClass}
+        value={added}
+        onClick={() => {
+          setAdded(!added);
+        }}
+      >
+        {added ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
